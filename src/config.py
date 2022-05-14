6856563ba2079f1fc270e16bf9853ec4a6e23c31
Kravchuk Roman <kravchuk.roman02@gmail.com>
2022-05-14 23:30:29 +0800

    added config class for queries

--- src/config.py
+++ src/config.py
@@ -5,8 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class Config:
+class FlaskConfig:
     SQLALCHEMY_DATABASE_URI = getenv('CONNECTION_STRING')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = getenv('SECRET_KEY')
-    
+
+
+class Config:
+    QUERY_LIMIT=5
