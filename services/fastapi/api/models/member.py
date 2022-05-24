febd71728d10b1a7340a0fb84b803a3c7b7d6938
romankravchuk <kravcuk.roman02@gmail.com>
2022-05-24 23:34:15 +0800

    feat: update member model

--- services/fastapi/api/models/member.py
+++ services/fastapi/api/models/member.py
@@ -8,5 +8,7 @@ class Member(Base):
     id = Column(BigInteger, primary_key=True)
     name = Column(String(100), nullable=False)
     discriminator = Column(Integer, nullable=False)
-    avatar_url = Column(String, nullable=False)
+    avatar = Column(String, nullable=False)
+    default_avatar = Column(String, nullable=False)
     created_at = Column(DateTime, nullable=False)
+    status = Column(String, nullable=False)
