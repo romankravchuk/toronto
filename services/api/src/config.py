d1d6de5b80883446f413821d07e9412097af6721
romankravchuk <kravcuk.roman02@gmail.com>
2022-05-25 00:01:54 +0800

    refactor: renamed fastapi to api

---
+++ services/api/src/config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from os import getenv
+
+
+load_dotenv()
+
+
+class Config(object):
+    CONNECTION_STRING = getenv('CONNECTION_STRING')
+    ACCESS_LOG_PATH = getenv('ACCESS_LOG_PATH')
+    ERROR_LOG_PATH = getenv('ERROR_LOG_PATH')
+    SOCKET_PATH = getenv('SOCKET_PATH')
---
+++ services/api/wsgi.py
@@ -0,0 +1,3 @@
+from src import create_api
+
+api = create_api()
