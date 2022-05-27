2c27a03f40de438ba9e4dcbc07a813e049199ee8
romankravchuk <kravcuk.roman02@gmail.com>
2022-05-28 00:52:34 +0800

    feat: added cogs names

--- services/discord-bot/src/config.py
+++ services/discord-bot/src/config.py
@@ -13,3 +13,4 @@ class Settings(object):
     CONNECTION_STRING = environ.get('DATABASE_URL')
     OWNERS = (324462804939833345, )
     GUILDS = (616889320841674753, 879709795760889937)
+    COGS = ['tasks']
